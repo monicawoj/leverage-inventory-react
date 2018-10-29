c9b8f41bc6466ea97dd1ac6170b75c5ccb802e25
Monica Wojciechowski <monica.wojciechowski@gmail.com>
2018-10-29 23:37:25 +0100

    testing dev api url on prod

--- app/containers/Routes/Routes.js
+++ app/containers/Routes/Routes.js
@@ -8,7 +8,7 @@ import NotFoundPage from 'containers/NotFoundPage/Loadable';
 const Routes = (parentProps) => (
   <Switch>
     <Route exact path="/your-leverage-inventory/your-results/" render={(props) => <UserResultsPage location={props.location} {...parentProps} />} />
-    <Route path="/your-leverage-inventory/your-results/print" render={(props) => <PrintUserResultsPage location={props.location} {...parentProps} />} />
+    <Route exact path="/your-leverage-inventory/your-results/print" render={(props) => <PrintUserResultsPage location={props.location} {...parentProps} />} />
     <Route exact path="/your-leverage-inventory/your-results/group-results" render={(props) => <ProfessorReportsPage location={props.location} {...parentProps} />} />
     {/* add other routes here */}
     <Route path="" component={NotFoundPage} />
