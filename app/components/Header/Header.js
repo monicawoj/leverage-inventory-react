4206b4099a38c671db40374cdcafe5e8d8c61f45
Monica Wojciechowski <monica.wojciechowski@gmail.com>
2018-10-30 09:52:36 +0100

    rerouting to new wp page

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -2,20 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import StyledHeader from './styles';
 
-// const redirectToPrintReport = () => {
-//   window.open('/wp-content/themes/make/leverage-inventory/src/js/index.html/your-leverage-inventory/your-results/print');
-//   // w.location = '/print';
-// };
-
 const Header = () => (
   <StyledHeader>
-    {/* <button
-      className="button"
-      onClick={() => redirectToPrintReport()}
-    >
-      Print Results
-    </button> */}
-    <Link to="/print" target="_blank">Print Results</Link>
     <Link to="/your-leverage-inventory/your-results/your-leverage-inventory-your-results-print/" target="_blank">Print Results</Link>
   </StyledHeader>
 );
