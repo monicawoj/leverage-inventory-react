300e7593dd5dba269c1aaeb2d76a4d44e43bc310
Monica Wojciechowski <monica.wojciechowski@gmail.com>
2018-10-31 20:44:39 +0100

    testing with hardcoded prod id

--- app/actions/api.js
+++ app/actions/api.js
@@ -13,8 +13,13 @@
 
 const getApiUrl = (id) => {
   const base = process.env.API_URL;
+  // test dev
   // return 'https://app.leverageinventory.com/backend/results?id=4260ccb2-60fe-46ba-8122-7ccd233cbf2d'
-  return `${base}?id=${id}`;
+  // test production
+  console.log(base);
+  console.log(id);
+  return 'https://leverage-inventory.wharton-research-programming.org/backend/results/?id=f0721981-f0a6-4127-af7c-9f00d3fc1f21';
+  // return `${base}?id=${id}`;
 }
 
 export default getApiUrl;
