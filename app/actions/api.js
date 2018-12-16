019337a31f01416246c6c5b156e6b78c93f5d603
Monica Wojciechowski <monica.wojciechowski@gmail.com>
2018-12-17 00:58:51 +0100

    rebuild

--- app/actions/api.js
+++ app/actions/api.js
@@ -7,8 +7,8 @@ const getApiUrl = (id) => {
   // console.log(base);
   // console.log(id);
   // return 'https://leverage-inventory.wharton-research-programming.org/backend/results/?id=f0721981-f0a6-4127-af7c-9f00d3fc1f21';
-  return 'https://leverage-inventory.wharton-research-programming.org/backend/results/?id=33333333333333333333333333333333';
-  return 'https://leverage-inventory.wharton-research-programming.org/backend/results/?id=11111111111111111111111111111111';
+  // return 'https://leverage-inventory.wharton-research-programming.org/backend/results/?id=33333333333333333333333333333333';
+  // return 'https://leverage-inventory.wharton-research-programming.org/backend/results/?id=11111111111111111111111111111111';
   return `${base}?id=${id}`;
 };
 
